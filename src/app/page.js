bf97902631edcfadbb9c77d2c0410e099798b5eb
maazkhurshid60 <maaz.khurshid60@gmail.com>
2024-07-18 15:04:00 +0500

    adding line for github activity

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 
 import {AllBlogsComponent} from '@/components/index'
 
-// changing this line for git activity 
+// adding this line for git activity 
 export default function Home() {
   return (
    <main>
