f5cf1ca8f76d473eb7a74a1b8507e06209547a5a
Vercel Access <vercelaccess@gmail.com>
2024-12-03 21:03:33 +0500

    Git activity

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 
 import { AllBlogsComponent } from '@/components/index'
 
-// Created this line for git activity 
+// Deleted this line for git activity 
 export default function Home() {
   return (
     <main>
