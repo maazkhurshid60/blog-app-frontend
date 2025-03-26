836fb21044e55f9fa6b2fe912c66697398ce232b
maazkhurshid60 <maaz.khurshid60@gmail.com>
2025-03-26 17:16:39 +0500

    GIT ACTIVITY: git activity performed

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 
 import { AllBlogsComponent } from '@/components/index'
 
-//Adding this line for git activity 
+//DELETING this line for git activity 
 export default function Home() {
   return (
     <main>
